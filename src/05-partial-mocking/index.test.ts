6b9f0ea5b4a52207cc488e0d2186813f8a232792
timashkov <timashkov060896@gmail.com>
2025-05-12 20:16:05 +0300

    fix/linter-warning-fix

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -3,6 +3,7 @@ import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 jest.mock('./index', () => {
   const originalModule =
     jest.requireActual<typeof import('./index')>('./index');
+
   return {
     ...originalModule,
     mockOne: jest.fn,
